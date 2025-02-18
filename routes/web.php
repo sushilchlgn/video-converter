3af03929dda34b88a550f75252bd35f91dbd651d
sushil chaulagain <sushilchlgn@gmail.com>
2025-02-18 23:14:15 +0545

    added video control route

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\VideoController;
+
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('upload', [VideoController::class, 'showUploadForm']);
+Route::post('upload', [VideoController::class, 'handleUpload'])->name('upload.video');
