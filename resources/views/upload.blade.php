ebe1be7aa774efb4c4ce945c0d5826f16e2daba4
sushil chaulagain <sushilchlgn@gmail.com>
2025-02-19 00:06:14 +0545

    allow users to select the desired output video format

--- resources/views/upload.blade.php
+++ resources/views/upload.blade.php
@@ -6,11 +6,31 @@
     <title>Upload Video</title>
 </head>
 <body>
+    <!-- Video upload form -->
     <form action="{{ route('upload.video') }}" method="POST" enctype="multipart/form-data">
         @csrf
         <label for="video">Choose a video file:</label>
-        <input type="file" name="video" id="video" required>
+        <input type="file" name="video" id="video" required><br>
+
+        <label for="format">Select output format:</label>
+        <select name="format" id="format" required>
+            <option value="mp4">MP4</option>
+            <option value="avi">AVI</option>
+            <option value="mkv">MKV</option>
+            <option value="flv">FLV</option>
+            <option value="mov">MOV</option>
+        </select><br>
+
         <button type="submit">Upload Video</button>
     </form>
+
+    <!-- Display success message and download link if available -->
+    @if(session('success'))
+        <p>{{ session('success') }}</p>
+    @endif
+
+    @if(session('downloadUrl'))
+        <p><a href="{{ session('downloadUrl') }}" download>Download Converted Video</a></p>
+    @endif
 </body>
 </html>
